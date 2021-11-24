01ccfa3b70fecb33cda765b0c818b7a61be1501f
Leonhard Seidel <leonhardseidel@gmx.de>
2021-11-24 13:09:26 +0100

    added basic program files

---
+++ Art-Net-4-PI.cpp
@@ -0,0 +1,14 @@
+﻿// Art-Net-4-PI.cpp : Defines the entry point for the application.
+//
+
+#include "Art-Net-4-PI.h"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	//test
+	return 0;
+}
---
+++ Art-Net-4-PI.h
@@ -0,0 +1,16 @@
+﻿// Art-Net-4-PI.h : Include file for standard system include files,
+// or project specific include files.
+
+//############################################################################
+//using Art-Net™ Designed by and Copyright Artistic Licence Holdings Ltd
+//############################################################################
+#pragma once
+
+#include <iostream>
+#include "Art-NetOemCodes.h" //pulled from https://artisticlicence.com/WebSiteMaster/Software/Art-Net/Art-NetOemCodes.h
+#include "Art-Net-Codes.h" //manually typed code macros from the latest Art-Net 4 documentation 
+#include "Art-Net-Structs.h" //manually typed struct definitions from the latest Art-Net 4 documentation 
+
+// TODO: Reference additional headers your program requires here.
+
+
